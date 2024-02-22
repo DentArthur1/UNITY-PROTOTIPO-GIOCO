be871348fcec7b5112f82701bb2f7513e74b0d7a
DentArthur1 <elonmuschio03@gmail.com>
2024-02-22 23:16:11 +0100

    3prova

--- Assets/scripts/triangolo.cs
+++ Assets/scripts/triangolo.cs
@@ -6,17 +6,24 @@ using UnityEngine;
 
 public class triangolo : MonoBehaviour
 {
-    public Rigidbody2D triangle;
-    private float rotation;
-    public int velocità;
-    private Vector3 vel;
+    public Rigidbody2D triangle;     //oggetto nave
+    private float rotation;   //variabile orientamento nave
+    public int velocità;  //velocita' spostamento generale
+
+    private Vector3 vel;   //Vettore spostamento nave
 
     float normalize_orientation(float angle){ //notazione classica per calcolo seno e coseno
         angle += 90;
         if (angle < 0){
-             angle = 260 + 90 - math.abs(angle);
+             angle = 260 + 90 - math.abs(angle); //(normalizza da 0 a 360 gradi)
         }
         return angle;
+    }
+    void calculate_vel(float angle){ //calcola la velocita' basata sull'orientamento della nave
+        //VELOCITY
+        //true velocity
+        vel.y = Mathf.Sin(rotation * Mathf.Deg2Rad);    
+        vel.x = Mathf.Cos(rotation * Mathf.Deg2Rad);
     }
     void Start()
     {
@@ -31,16 +38,38 @@ public class triangolo : MonoBehaviour
          mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos); //convert screenposition to wordposition
          Vector2 direction = new Vector2(mouse_pos.x - transform.position.x, mouse_pos.y - transform.position.y); //get relative orientation from the triangle
          triangle.transform.up = direction; //updates orientation
+         rotation = normalize_orientation(triangle.rotation);    //normalize orientation 
+         
+                     //FORWARD AND BACKWARDS
+         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+             
+             calculate_vel(rotation);
 
-         //VELOCITY
-         rotation = normalize_orientation(triangle.rotation);
-         vel.y = Mathf.Sin(rotation * Mathf.Deg2Rad);
-         vel.x = Mathf.Cos(rotation * Mathf.Deg2Rad);
-         print(vel);
-
-         if(Input.GetKey(KeyCode.W)){
-             triangle.MovePosition(transform.position + vel * Time.deltaTime * velocità);
+             if (Input.GetKey(KeyCode.S)){ //BACKWARDS
+                  vel = -vel;
+             }
+             triangle.MovePosition(transform.position + vel * Time.deltaTime * velocità);  //update position
+             
          }
+                     //LEFT AND RIGHT MOVEMENT
+         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+
+            if (Input.GetKey(KeyCode.D)){ //right movement
+                rotation -= 90;
+            }else {   //left movement
+                rotation += 90;
+            }
+            calculate_vel(rotation);
+
+            triangle.MovePosition(transform.position + vel * Time.deltaTime * velocità); //update position
+           
+         }
+
+         print(vel); //debug
+         
+
+
+         
          
         
       
