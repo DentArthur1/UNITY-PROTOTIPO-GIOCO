4860e6faaf6d5239f2bae16ea09179dd92a7bca5
DentArthur1 <elonmuschio03@gmail.com>
2024-03-03 16:15:42 +0100

    DirFinder Prototipo (FIX)

--- Assets/scripts/DirectionIndicator.cs
+++ Assets/scripts/DirectionIndicator.cs
@@ -18,6 +18,7 @@ public class DirectionIndicator : MonoBehaviour //CLASSE PER GESTIRE IL MOVIMENT
     public int anchor_y; //coordinate y posizione freccia sullo schermo
     private float scale_x; //dimensione_x originale freccia
     private float scale_y; //dimensione_y originale freccia
+    private float actual_scale_y; //dimensione_y attuale freccia
     private float cam_stock; //zoom iniziale camera
     private float mult_factor; //variabile di moltiplicazione scala oggetto freccia
     private float max_vec_magnitude; //limite massimo di magnitudine vettore velocita'
@@ -38,7 +39,7 @@ public class DirectionIndicator : MonoBehaviour //CLASSE PER GESTIRE IL MOVIMENT
         anchor_arrows();
         scale_arrow();
         rotate_arrows();
-        magnitude();
+        //magnitude();
     }
     void anchor_arrows()
     {
@@ -49,10 +50,10 @@ public class DirectionIndicator : MonoBehaviour //CLASSE PER GESTIRE IL MOVIMENT
     }
     void scale_arrow() { //scala la grandezza della freccia in modo che sia costante ai cambiamenti di zoom
         mult_factor = (cam.orthographicSize / cam_stock);
-        arrow.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * mult_factor, arrow.transform.localScale.z);
-        engine.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * mult_factor, engine.transform.localScale.z);
-        thruster.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * mult_factor, thruster.transform.localScale.z);
-        expected.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * mult_factor, expected.transform.localScale.z);
+        arrow.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * fun.remap_value(ship.velocity.magnitude, 0, max_vec_magnitude, 0, 1) * mult_factor, arrow.transform.localScale.z);
+        engine.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * fun.remap_value((triangle.engine_vel * fun.partition_vect(triangle.direction)).magnitude, 0, max_vec_magnitude, 0, 1) * mult_factor, engine.transform.localScale.z);
+        thruster.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * fun.remap_value((triangle.thruster_vel * fun.partition_vect(triangle.direction)).magnitude, 0, max_vec_magnitude, 0, 1) * mult_factor, thruster.transform.localScale.z);
+        expected.transform.localScale = new Vector3(scale_x * mult_factor, scale_y * fun.remap_value(triangle.vel.magnitude, 0, max_vec_magnitude, 0, 1) * mult_factor, expected.transform.localScale.z);
     }
 
     void rotate_arrows() //ruota la freccia per indicare la direzione attuale della nave
